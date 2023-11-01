3fc8f9a90216cdf120570a548db2b5afa6cd38d8
Alyssa H <alyssa_hondrade@icloud.com>
2023-11-01 18:59:40 +0800

    created map with basic markers

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -2,8 +2,9 @@
 let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
 
 // Define map parameters
-let australia_coords = [];
-let map_zoom = 8;
+// let australia_coords = [-25.2744, 133.7751]; // australia
+let map_centre = [39.0000, 34.0000];
+let map_zoom = 3;
 
 function create_map(layer) {
     // Create the map background tile layer
@@ -25,9 +26,9 @@ function create_map(layer) {
 
     // Create the map
     let my_map = L.map("map", {
-        center: australia_coords,
+        center: map_centre,
         zoom: map_zoom,
-        labels: [map_background, layer]
+        layers: [map_background, layer]
     });
 
     // Create layer control and add to the map
@@ -40,19 +41,30 @@ function create_markers(response) {
     // Pull the "features" property from the response
     let feature = response.features;
 
+    // Initialise the array to hold the markers
+    let earthquake_markers = [];
+    
     for (let i=0; i<feature.length; i++) {
         console.log(feature[i]);
 
         // Parse the "coordinates" property
         let lat = feature[i].geometry.coordinates[1];
-        let lon = feature[i].geometry.coodrinates[0];
+        let lon = feature[i].geometry.coordinates[0];
         let depth = feature[i].geometry.coordinates[2];
 
         // Get the "magnitude"
-        let mag = feature[i].property.mag;
+        let mag = feature[i].properties.mag;
 
-        
+        // Create the marker
+        let marker = L.marker([lat, lon]);
+        earthquake_markers.push(marker);
     };
+
+    // Create a layer group made from the marker array
+    let markers_layer = L.layerGroup(earthquake_markers);
+
+    // Call the create_map() function
+    create_map(markers_layer);
 };
 
 
