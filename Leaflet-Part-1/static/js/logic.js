e9dcb400e811f362fda55e313bb8f3f0a9cc88c6
Alyssa H <alyssa_hondrade@icloud.com>
2023-11-01 18:40:34 +0800

    added skeleton function for creating the maps

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,13 +1,57 @@
 // USGS - All month, all earthquakes
 let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
 
+// Define map parameters
+let australia_coords = [];
+let map_zoom = 8;
+
+function create_map(layer) {
+    // Create the map background tile layer
+    let map_background = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: 'abcd',
+        maxZoom: 20
+    });
+
+    // Create base_maps object
+    let base_maps = {
+        Background: map_background
+    };
+
+    // Create overlay_maps object
+    let overlay_maps = {
+        Earthquakes: layer
+    };
+
+    // Create the map
+    let my_map = L.map("map", {
+        center: australia_coords,
+        zoom: map_zoom,
+        labels: [map_background, layer]
+    });
+
+    // Create layer control and add to the map
+    L.control.layers(base_maps, overlay_maps).addTo(my_map);
+};
+
+
 function create_markers(response) {
-    // console.log(response.features);
 
-    let features = response.features;
+    // Pull the "features" property from the response
+    let feature = response.features;
 
-    for (let i=0; i<features.length; i++) {
-        console.log(features[i]);
+    for (let i=0; i<feature.length; i++) {
+        console.log(feature[i]);
+
+        // Parse the "coordinates" property
+        let lat = feature[i].geometry.coordinates[1];
+        let lon = feature[i].geometry.coodrinates[0];
+        let depth = feature[i].geometry.coordinates[2];
+
+        // Get the "magnitude"
+        let mag = feature[i].property.mag;
+
+        
     };
 };
 
